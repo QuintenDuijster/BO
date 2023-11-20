3f89a66208caf5c80c10c1d4c62eec48c080d0eb
Entropire <36831@ma-web.nl>
2023-11-20 14:10:35 +0100

    hier

--- Website/museum online website/src/App.tsx
+++ Website/museum online website/src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () =>
+{
+    return (
+        <div className="App">
+            <header>
+                <ul>
+                    <li><a href="#test1">test</a></li>
+                    <li className="dropdown-1">
+                        <span>text</span>
+                        <div className="dropdown-content" id="dropdown1-content">
+                            <ul>
+                                <li><a href="#test1">test</a></li>
+                                <li><a href="#test2">test</a></li>
+                                <li><a href="#test3">test</a></li>
+                                <li><a href="#test4">test</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li className="dropdown-2">
+                        <span>text</span>
+                        <div className="dropdown-content" id="dropdown2-content">
+                            <ul>
+                                <li><a href="#test1">test</a></li>
+                                <li><a href="#test2">test</a></li>
+                                <li><a href="#test3">test</a></li>
+                                <li><a href="#test4">test</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li><a href="#test4">test</a></li>
+                    <li><a href="#test5">test</a></li>
+                </ul>
+            </header>
+        </div>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
