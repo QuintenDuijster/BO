1a2d2ecf0332318c080a747bf85d7454c9b94a2a
Nora_de_Greeuw <37214@ma-web.nl>
2023-11-20 15:03:29 +0100

    subpage maar eigg alleen refs

---
+++ Website/museum online website/src/faq.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import App from './App.tsx'
+import './faq.css'
+
+const Faq: React.FC = () => {
+    return (
+        <div className="App">
+        <App/>
+        </div>
+    );
+};
+
+export default Faq;
