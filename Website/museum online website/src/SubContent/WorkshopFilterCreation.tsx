997707144312c51288ce655a7bb669714141a600
Nxbi <37214@ma-web.nl>
2023-11-22 18:41:02 +0100

    wave

--- Website/museum online website/src/SubContent/WorkshopFilterCreation.tsx
+++ Website/museum online website/src/SubContent/WorkshopFilterCreation.tsx
@@ -2,40 +2,80 @@ import React from 'react';
 import Header from '../Herbruikbaar/Header.tsx';
 import Footer from '../Herbruikbaar/Footer.tsx';
 
-const WorkshopFilterCreation: React.FC = () =>
-{
+const WorkshopFilterCreation: React.FC = () => {
     return (
         <div className="FilterCreation">
             <Header />
             <main>
                 <div className="Info">
-                    <h2>WHAT WILL YOU GET UP TO?</h2>
-                    <ul>
-                        <li>
-                            Introduction Lecture on AR filters and masks used in social media, the arts, and various cultures, given by specialists in the field.                           
-                        </li>
-                        <li>
-                            Museum exhibition visit – explore connection to the 10 large scale new media artworks in the current exhibition: ‘Life in a Different Resolution’ and Realtime
-                        </li>
-                        <li>
-                            AR Filter creation workshop in “Lensstudio” (software)
-                        </li>
-                        <div className="WhatToLearn">
-                            <h2>WAT GA JE LEREN?</h2>
+                    <h1>Filter creation</h1>
+                    <p></p>
+                    <div className="VerwachtingWorkshop">
+                        <h2>WAT KUN JE VERWACHTEN?</h2>
+                        <ul>
+                            <li>
+                                Introductie door vakspecialisten over AR-filters en het gebruik van maskers in sociale media, kunst en verschillende culturen.
+                            </li>
+                            <li>
+                                Museumbezoek - ontdek de verbinding met de tien grootschalige nieuwe media kunstwerken in de huidige tentoonstelling: UFO-Unidentified Fluid Other
+                            </li>
+                            <li>
+                                Workshop AR-filter maken in Lens Studio (software).
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="WhatToLearn">
+                        <h2>WAT GA JE LEREN?</h2>
 
-                            <ul>
-                                <li>
-                                    Software vaardigheden en AR-filter maken met Lens Studio
-                                </li>
-                                <li>
-                                    Kritisch reflecteren op het gebruik van gezichtsfilters en maskers
-                                </li>
-                            </ul>
-                        </div>
-                    </ul>
+                        <ul>
+                            <li>
+                                Software vaardigheden en AR-filter maken met Lens Studio
+                            </li>
+                            <li>
+                                Kritisch reflecteren op het gebruik van gezichtsfilters en maskers
+                            </li>
+                            <li>
+                                De geschiedenis en theorie van maskeren
+                            </li>
+                            <li>
+                                Creatieve expressie door middel van gezichtsfilters
+                            </li>
+                            <li>
+                                De impact van nieuwe technologieën op de samenleving en cultuur
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="GoodToKnow">
+                        <h2>GOED OM TE WETEN</h2>
+                        <ul>
+                            <li>
+                                Max 30 leerlingen/studenten per workshop
+                            </li>
+                            <li>
+                                Programma van vier uur (0.5 uur introductie, 1 uur museumbezoek, 2.5 uur workshop)
+                            </li>
+                            <li>
+                                Neem je eigen laptop mee
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="Kosten">
+                        <h2>KOSTEN: (inclusief museumbezoek)</h2>
+                        <ul>
+                            <li>
+                                Basisscholen en middelbare scholen: €175 + €40 per persoon
+                            </li>
+                            <li>
+                                Studenten: €175 + €43/per persoon
+                            </li>
+                            <li>
+                                Bedrijven: €200 + €50/per persoon
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </main>
-        <Footer/>
+            <Footer />
         </div>
     );
 };
--- Website/museum online website/src/SubContent/WorkshopSlowLooking.tsx
+++ Website/museum online website/src/SubContent/WorkshopSlowLooking.tsx
@@ -8,21 +8,14 @@ const WorkshopSlowLooking: React.FC = () => {
         <div className="Workshop">
 
             <Header />
-
             <main>
-
                 <div className="Info">
-
                     <h1>Slow Looking</h1>
-
                     <p>
                         <img src="Images/WorkshopImg/NXT_Random-International_PR_Riccardo-De-Vecchi-photographer-12-1024x778.jpg" alt="The developer isn't good at coding here should be an image" />
                     </p>
-
                     <div className="VerwachtingWorkshop">
-
                         <h2>WAT TE VERWACHTEN?</h2>
-
                         <ul>
                             <li>
                                 Discover & Design: Hoe is uw huidige organisatie vormgegeven en waar wilt u naartoe?
@@ -34,23 +27,19 @@ const WorkshopSlowLooking: React.FC = () => {
                                 Engage & Act: Hoe breng je je nieuwe gedrag binnen de organisatie tot stand en hoe daag je jezelf uit om te blijven innoveren?
                             </li>
                         </ul>
-
                     </div>
-
                     <div className="WhatToLearn">
-
                         <h1>WAT GA JE LEREN?</h1>
-
                         <ul>
                             <li>
                                 Vertraag: om de wereld goed te observeren
                                 Maak kennis met de concepten van aandachtig kijken en zorgvuldig luisteren
-                                Ontdek hoe je past in grotere (sociale) constructies zoals school, sport of werk                                
+                                Ontdek hoe je past in grotere (sociale) constructies zoals school, sport of werk
                             </li>
                             <li>
                                 Deel: wissel verhalen uit over mensen, plaats en identiteit
                                 Leer hoe je verhalen op een betekenisvolle manier kunt verzamelen en vertellen
-                                Maak kennis met verschillende communicatiemodellen en manieren van interactie                                
+                                Maak kennis met verschillende communicatiemodellen en manieren van interactie
                             </li>
                             <li>
                                 Leer: over hoe jouw eigen leven in verbinding staat met grotere menselijke verhalen
@@ -58,13 +47,9 @@ const WorkshopSlowLooking: React.FC = () => {
                                 Krijg nieuwe inzichten met betrekking tot je dagelijks leven, gemeenschappen en identiteiten zowel online en offline
                             </li>
                         </ul>
-
                     </div>
-
                     <div className="GoodToKnow">
-
                         <h2>GOED OM TE WETEN:</h2>
-
                         <ul>
                             <li>
                                 4 uur programma in Nxt Museum
@@ -76,13 +61,9 @@ const WorkshopSlowLooking: React.FC = () => {
                                 Programma wordt afgestemd op de relevante thema’s binnen uw organisatie
                             </li>
                         </ul>
-
                     </div>
-
                     <div className="Kosten">
-
                         <h2>KOSTEN (inclusief museumbezoek):</h2>
-
                         <ul>
                             <li>
                                 Basisscholen en middelbare scholen: €130 + €25 per persoon
@@ -94,16 +75,11 @@ const WorkshopSlowLooking: React.FC = () => {
                                 Bedrijven: €130 + €39,75 per persoon
                             </li>
                         </ul>
-
                     </div>
-
                 </div>
-
                //Form voor het aanmelden van workshop
-
                 <Footer />
             </main>
-
         </div>
     );
 };
