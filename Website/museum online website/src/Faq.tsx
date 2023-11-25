265e3faa9b1c47bfa6a2a2c59af9fc242b00b7d1
Nxbi <37214@ma-web.nl>
2023-11-25 15:38:21 +0100

    Made faq less empty
    loop vast op goede structuur

--- Website/museum online website/src/Faq.tsx
+++ Website/museum online website/src/Faq.tsx
@@ -1,1 +1,27 @@
-﻿
+﻿import React from 'react';
+import Header from './Herbruikbaar/Header.tsx';
+import Footer from './Herbruikbaar/Footer.tsx';
+
+const FAQ: React.FC = () => {
+    return (
+        <div className="Faq">
+
+            <Header />
+            <main>
+                <div className="Faq">
+                    <h1>Veelgestelde vragen</h1>
+                    <p>Hier vindt u onze meest gestelde vragen</p>
+
+                    <div className="Tickets">
+                        <button onClick={ }>Tickets</button>
+                    </div>
+                </div>
+            </main>
+
+            <Footer />
+
+        </div>
+    );
+};
+
+export default FAQ;
