b940c8c0ad0450b1d62b287b2f6962ecdadca3e8
Quinten <146432390+QuintenDuijster@users.noreply.github.com>
2024-03-17 23:07:55 +0100

    .

--- Assets/Script/Player/Controller.cs
+++ Assets/Script/Player/Controller.cs
@@ -3,58 +3,66 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Controller : MonoBehaviour
 {
+	[SerializeField] private GameObject map;
+    [SerializeField] private float rotationSpeed = 100f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float runningSpeed = 10f;
+    [SerializeField] private float walkingSpeed = 5f;
+    private float moveSpeed = 0f;
+    private Rigidbody rb;
 
-	[SerializeField] private float rotationSpeed = 100f;
-	[SerializeField] private float jumpForce = 5f;
-	[SerializeField] private float runningSpeed = 10f;
-	[SerializeField] private float walkingSpeed = 5f;
-	private float moveSpeed = 0f;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
-	private Rigidbody rb;
+    private void Update()
+    {
+        RotateWithMouse();
 
-	private void Start()
-	{
-		rb = GetComponent<Rigidbody>();
-	}
+        if (Input.GetButtonUp("Jump") && isGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
 
-	private void Update()
-	{
-		RotateWithMouse();
+        bool isGrounded()
+        {
+            return Physics.Raycast(transform.position, -Vector3.up, 3f);
+        }
+    }
 
-		if (Input.GetButtonUp("Jump") && isGrounded())
-		{
-			rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-		}
+    private void FixedUpdate()
+    {
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
 
-		bool isGrounded()
-		{
-			return Physics.Raycast(transform.position, -Vector3.up, 3f);
-		}
-	}
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = runningSpeed;
+        }
 
-	private void FixedUpdate()
-	{
-		float Horizontal = Input.GetAxis("Horizontal");
-		float Vertical = Input.GetAxis("Vertical");
+        else
+        {
+            moveSpeed = walkingSpeed;
+        }
 
-		if (Input.GetKey(KeyCode.LeftShift))
-		{
-			moveSpeed = runningSpeed;
-		}
-		else
-		{
-			moveSpeed = walkingSpeed;
-		}
+        if (Horizontal != 0 && Vertical != 0)
+        {
+            moveSpeed = moveSpeed / 2;
+        }
 
-		Vector3 movement = new Vector3(Horizontal, 0f, Vertical).normalized * moveSpeed;
-		rb.MovePosition(rb.position + transform.TransformDirection(movement) * Time.fixedDeltaTime);
-	}
+        Vector3 playerMovement = new Vector3(Horizontal, 0f, Vertical).normalized * moveSpeed;
+        rb.MovePosition(rb.position + transform.TransformDirection(playerMovement) * Time.fixedDeltaTime);
 
-	private void RotateWithMouse()
-	{
-		float mouseX = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
-		Quaternion deltaRotation = Quaternion.Euler(Vector3.up * mouseX);
-		rb.MoveRotation(rb.rotation * deltaRotation);
-	}
+        Vector3 mapMovement = new Vector3(Horizontal, 0f, Vertical).normalized * moveSpeed;
+        map.transform.position -= transform.TransformDirection(mapMovement) * Time.fixedDeltaTime;
+    }
+
+    private void RotateWithMouse()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+        Quaternion deltaRotation = Quaternion.Euler(Vector3.up * mouseX);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }
 }
 
--- Assets/Script/mapGenerator.cs
+++ Assets/Script/mapGenerator.cs
@@ -1,33 +1,54 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class mapGenerator : MonoBehaviour
 {
-    [SerializeField] private GameObject[] sections;
+    [SerializeField] private GameObject[] preSetSections;
     [SerializeField] private Transform player;
 	[SerializeField] private int startPlatformLenght;
+    [SerializeField] private GameObject map;
 	private float zIndex = 0;
 	private float zLocation = 0;
+	private List<GameObject> loadedSectons = new List<GameObject>();
 
-	private void Start()
+    private void Start()
 	{
+        zIndex = -20 * (float)Math.Floor(startPlatformLenght * 0.35);
+
 		for (int i = 0; i < startPlatformLenght; i++)
 		{
-			int randomSection = Random.Range(0, sections.Length);
-			sections[randomSection].transform.position = new Vector3(0, 0, zIndex + 20 * i);
-			Instantiate(sections[randomSection]);
+			int randomSection = UnityEngine.Random.Range(0, preSetSections.Length);
+            Vector3 newPosition = new Vector3(0, 0, zIndex + (20 * i));
+            GameObject newSection = Instantiate(preSetSections[randomSection], newPosition, Quaternion.identity);
+            newSection.transform.SetParent(map.transform);
+            loadedSectons.Add(newSection);
 		}
 
-		zLocation = 20 * startPlatformLenght;
+		zLocation = 20 * (startPlatformLenght - 1);
 	}
 
-	private void Update()
-	{
-		if (player.position.z > zIndex)
-		{
-			int randomSection = Random.Range(0, sections.Length);
-			sections[randomSection].transform.position = new Vector3(0, 0, zIndex + zLocation);
-			Instantiate(sections[randomSection]);
-			zIndex += 20;
-		}
-	}
+    private void Update()
+    {
+        if (map.transform.position.z <= -20)
+        {
+            int randomSection = UnityEngine.Random.Range(0, preSetSections.Length);
+            Vector3 newPosition = new Vector3(0, 0, zIndex + zLocation);
+            GameObject newSection = Instantiate(preSetSections[randomSection], newPosition, Quaternion.identity);
+            newSection.transform.SetParent(map.transform);
+
+            GameObject removedObject = loadedSectons[0];
+            loadedSectons.RemoveAt(0);
+            Destroy(removedObject);
+
+            loadedSectons.Add(newSection);
+
+            for (int i = 0; i < loadedSectons.Count; i++)
+            {
+                loadedSectons[i].transform.position = new Vector3(0f, 0f, loadedSectons[i].transform.position.z - 20);
+            }
+
+            map.transform.position = new Vector3(0f, 0f, 0f);
+        }
+    }
 }
